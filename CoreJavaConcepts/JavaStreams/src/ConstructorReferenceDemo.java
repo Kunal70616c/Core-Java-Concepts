ad3e7a67b824cf3f93314b8fd4e992f620667cc7
raiko <kunal.cs.dev@outlook.com>
2026-01-18 00:38:27 +0530

    Constructor and Method Reference

--- CoreJavaConcepts/JavaStreams/src/ConstructorReferenceDemo.java
+++ CoreJavaConcepts/JavaStreams/src/ConstructorReferenceDemo.java
@@ -5,11 +5,11 @@ import java.util.stream.Collectors;
 public class ConstructorReferenceDemo {
     public static void main(String[] args) {
         //Constructor Reference
-        //::
         List<String> list = List.of("Apple", "Banana", "Cherry");
         list.forEach(System.out::println);
-        List<String> brandNames = Arrays.asList("Samsung", "Apple", "OneMinus");
+        List<String> brandNames = Arrays.asList("Samsung", "Apple", "OneMinus"); // Lambda
         List<Mobile> mobiles = brandNames.stream().map(Mobile::new).collect(Collectors.toList());
+        // Constructor Reference
         mobiles.forEach(System.out::println);
 
     }
